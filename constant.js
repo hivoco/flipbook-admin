bcccf729ffe51628ad61c54f52a9ea8bc30af378
ranjanhivoco <ranjan@hivoco.com>
2025-06-04 16:22:35 +0530

    added changes

--- constant.js
+++ constant.js
@@ -4,4 +4,4 @@ https://flipbook.api.thefirstimpression.ai`;
 export const USER_FACING_URL = "https://flipbook.thefirstimpression.ai";
 
 export const EMAIL = "admin@flipbook.com";
-export const PASSWORD = "password";
+export const PASSWORD = "@flipbook";
