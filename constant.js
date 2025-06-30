a7d0d9d661f39be1a927e7eeded48fa547778b79
ranjanhivoco <ranjan@hivoco.com>
2025-06-30 18:49:36 +0530

    added features

--- constant.js
+++ constant.js
@@ -1,9 +1,9 @@
-// export const BASE_URL = `
-// https://flipbook.api.thefirstimpression.ai`;
+export const BASE_URL = `
+https://flipbook.api.thefirstimpression.ai`;
 
 // export const BASE_URL = 'http://192.168.1.12:8831';
 
-export const BASE_URL = "http://localhost:8831";
+// export const BASE_URL = "http://localhost:8831";
 
 export const USER_FACING_URL = "https://flipbook.thefirstimpression.ai";
 
